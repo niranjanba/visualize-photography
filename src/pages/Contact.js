2d98b48603d7f130b68676f0eb73c25d0214155c
Niranjan B A <niranjangowda821@gmail.com>
2022-06-15 14:05:40 +0530

    added og tags

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,6 +7,7 @@ import "../Contact.css";
 import { ImLocation } from "react-icons/im";
 import { MdCall } from "react-icons/md";
 import { HiMail } from "react-icons/hi";
+import logo from "../images/logo.png";
 
 function Contact() {
     const makePhoneCall = () => {
@@ -66,12 +67,13 @@ function Contact() {
                 service: formValues.service,
                 message: formValues.message,
             };
+            console.log(body);
             fetch("/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
                 },
-                body: encode({ "form-name": "contact", ...body }),
+                body: encode({ "form-name": "contact-form", ...body }),
             })
                 .then(() => alert("Success!"))
                 .catch((error) => alert(error));
@@ -138,174 +140,193 @@ function Contact() {
             ) : (
                 ""
             )}
-            <div className="container">
-                <div className="heading">
-                    <h2 className="h1 mb-3 mt-3 text-center underline">
-                        Contact Us
-                    </h2>
-                    <h4>Let's Start a Conversation</h4>
-                </div>
-                <div className="row mobile-contact">
-                    <div className="col-lg-5 d-flex align-items-stretch">
-                        <div className="info">
-                            <div className="address">
-                                <i>
-                                    <ImLocation />
-                                </i>
-                                <h4>Location:</h4>
-                                <p>Bangalore, India</p>
-                            </div>
+            <div className="wrapper" style={{ background: "#00000075" }}>
+                <div className="container">
+                    <div className="heading">
+                        <img src={logo} alt="logo" style={{ width: "40%" }} />
+                        <h2 className="h1 mb-3 mt-3 text-center underline">
+                            Contact Us
+                        </h2>
+                        <h4>Let's Start a Conversation</h4>
+                    </div>
+                    <div className="row mobile-contact">
+                        <div className="col-lg-5 d-flex align-items-stretch">
+                            <div className="info">
+                                <div className="address">
+                                    <i>
+                                        <ImLocation />
+                                    </i>
+                                    <h4>Location:</h4>
+                                    <p>Bangalore, India</p>
+                                </div>
 
-                            <div className="email">
-                                <i>
-                                    <HiMail />
-                                </i>
-                                <h4>Email:</h4>
-                                <p>
-                                    <a
-                                        style={{ color: "black" }}
-                                        href="mailto:visualize.photos@gmail.com"
-                                    >
-                                        visualize.photos@gmail.com
-                                    </a>
-                                </p>
-                            </div>
+                                <div className="email">
+                                    <i>
+                                        <HiMail />
+                                    </i>
+                                    <h4>Email:</h4>
+                                    <p>
+                                        <a
+                                            style={{ color: "black" }}
+                                            href="mailto:visualize.photos@gmail.com"
+                                        >
+                                            visualize.photos@gmail.com
+                                        </a>
+                                    </p>
+                                </div>
 
-                            <div className="phone">
-                                <i>
-                                    <MdCall />
-                                </i>
-                                <h4>Call:</h4>
-                                <p onClick={makePhoneCall}>+91 9980378466</p>
-                            </div>
-                            <div className="mapouter">
-                                <div className="gmap_canvas">
-                                    <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.012600898512!2d77.49070341384429!3d13.034869417015967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3d778c80e4ab%3A0x10bcdeaf78f0e105!2sHydro%20E11%20Studios!5e0!3m2!1sen!2sin!4v1636468529837!5m2!1sen!2sin"
-                                        frameBorder="0"
-                                        style={{
-                                            border: "0",
-                                            width: "100%",
-                                            height: "290px",
-                                        }}
-                                        allowFullScreen
-                                    ></iframe>
+                                <div className="phone">
+                                    <i>
+                                        <MdCall />
+                                    </i>
+                                    <h4>Call:</h4>
+                                    <p onClick={makePhoneCall}>
+                                        +91 9980378466
+                                    </p>
+                                </div>
+                                <div className="mapouter">
+                                    <div className="gmap_canvas">
+                                        <iframe
+                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.012600898512!2d77.49070341384429!3d13.034869417015967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3d778c80e4ab%3A0x10bcdeaf78f0e105!2sHydro%20E11%20Studios!5e0!3m2!1sen!2sin!4v1636468529837!5m2!1sen!2sin"
+                                            frameBorder="0"
+                                            style={{
+                                                border: "0",
+                                                width: "100%",
+                                                height: "290px",
+                                            }}
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div
-                        className="
+                        <div
+                            className="
                                 col-lg-7
                                 d-flex
                                 mt-5 mt-lg-0
                                 align-items-stretch align-items-center
                             "
-                    >
-                        <form
-                            className="php-email-form"
-                            onSubmit={handleSubmit}
-                            ref={formRef}
-                            data-netlify={true}
                         >
-                            <div className="form-row contact-details">
-                                <div className="form-group w-100">
-                                    <label htmlFor="name">Your Name</label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        className="form-control"
-                                        id="name"
-                                        onChange={handleChange}
-                                    />
-                                    <div className="validate">
-                                        {formErrors.name}
+                            <form
+                                className="php-email-form"
+                                onSubmit={handleSubmit}
+                                ref={formRef}
+                            >
+                                <div className="form-row contact-details">
+                                    <div className="form-group w-100">
+                                        <label htmlFor="name">Your Name</label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            className="form-control"
+                                            id="name"
+                                            onChange={handleChange}
+                                        />
+                                        <div className="validate">
+                                            {formErrors.name}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="form-group w-100">
-                                    <label htmlFor="name">Your Email</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="email"
-                                        id="email"
-                                        onChange={handleChange}
-                                    />
-                                    <div className="validate">
-                                        {formErrors.email}
+                                    <div className="form-group w-100">
+                                        <label htmlFor="name">Your Email</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="email"
+                                            id="email"
+                                            onChange={handleChange}
+                                        />
+                                        <div className="validate">
+                                            {formErrors.email}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="form-group w-100">
-                                    <label htmlFor="name">
-                                        Your Phone Number
-                                    </label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="phone"
-                                        id="phone"
-                                        onChange={handleChange}
-                                    />
-                                    <div className="validate">
-                                        {formErrors.phone}
+                                    <div className="form-group w-100">
+                                        <label htmlFor="name">
+                                            Your Phone Number
+                                        </label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="phone"
+                                            id="phone"
+                                            onChange={handleChange}
+                                        />
+                                        <div className="validate">
+                                            {formErrors.phone}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="form-group w-100">
-                                    <label
-                                        htmlFor="service"
-                                        onChange={handleChange}
-                                    >
-                                        Select Service
-                                    </label>
-                                    <select
-                                        id="service"
-                                        className="form-select"
-                                        aria-label="Default select example"
-                                        name="service"
-                                        defaultValue="Select Service"
-                                        onChange={handleChange}
-                                    >
-                                        <option value="Select Service" disabled>
+                                    <div className="form-group w-100">
+                                        <label
+                                            htmlFor="service"
+                                            onChange={handleChange}
+                                        >
                                             Select Service
-                                        </option>
-                                        <option value="Tax Services">
-                                            Wedding Photography
-                                        </option>
-                                        <option value="Management Services">
-                                            Birthday Photography
-                                        </option>
-                                        <option value="Accounting Services">
-                                            Kids Photography
-                                        </option>
-                                        <option value="Legal Services">
-                                            Events Photography
-                                        </option>
-                                    </select>
-                                    <div className="validate">
-                                        {formErrors.service}
+                                        </label>
+                                        <select
+                                            id="service"
+                                            className="form-select"
+                                            aria-label="Default select example"
+                                            name="service"
+                                            defaultValue="Select Service"
+                                            onChange={handleChange}
+                                        >
+                                            <option
+                                                value="Select Service"
+                                                disabled
+                                            >
+                                                Select Service
+                                            </option>
+                                            <option value="wedding photography">
+                                                Wedding Photography
+                                            </option>
+                                            <option value="Pre Wedding Photoshoot">
+                                                Pre-Wedding Photography
+                                            </option>
+                                            <option value="birthday photography">
+                                                Birthday Photography
+                                            </option>
+                                            <option value="kids photography">
+                                                Kids Photography
+                                            </option>
+                                            <option value="events photography">
+                                                Events Photography
+                                            </option>
+                                            <option value="traditional phtography">
+                                                Traditional Photography
+                                            </option>
+                                            <option value="Baby shower">
+                                                Baby shower Photoshoot
+                                            </option>
+                                            <option value="candid photoshoot">
+                                                Candid Photoshoot
+                                            </option>
+                                        </select>
+                                        <div className="validate">
+                                            {formErrors.service}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="name">Message</label>
-                                <textarea
-                                    className="form-control"
-                                    name="message"
-                                    id="message"
-                                    rows="10"
-                                    onChange={handleChange}
-                                ></textarea>
-                                <div className="validate">
-                                    {formErrors.message}
+                                <div className="form-group">
+                                    <label htmlFor="name">Message</label>
+                                    <textarea
+                                        className="form-control"
+                                        name="message"
+                                        id="message"
+                                        rows="10"
+                                        onChange={handleChange}
+                                    ></textarea>
+                                    <div className="validate">
+                                        {formErrors.message}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="text-center">
-                                <button className="submit" type="submit">
-                                    Request a callback
-                                </button>
-                            </div>
-                        </form>
+                                <div className="text-center">
+                                    <button className="submit" type="submit">
+                                        Request a callback
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
