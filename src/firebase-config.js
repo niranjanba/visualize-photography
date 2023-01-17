09430822436626bf1883d4e172ae187cfe3e5e07
Niranjan B A <63544929+niranjanba@users.noreply.github.com>
2023-01-17 12:40:30 +0000

    remove console.log and added process.envs

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,11 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
-console.log(process.env.REACT_APP_API_KEY);
 const firebaseConfig = {
-    apiKey: 'AIzaSyDAaZjhJhdK2NSRgYmh-WncsLTJ8OGk-PY',
-    authDomain: 'visualize-photography.firebaseapp.com',
-    projectId: "visualize-photography",
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_APP_ID}`,
     storageBucket: "visualize-photography.appspot.com",
     messagingSenderId: "888101784503",
     appId: '1:888101784503:web:3277776b2bf5f2dd903632',
